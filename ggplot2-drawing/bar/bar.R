5f0be7229e35c2cb4fcc20d6d724b486b967cf85
zhengzhouxia <35135685+zhengzhouxia@users.noreply.github.com>
2018-09-13 10:14:13 +0800

    Update bar.R

--- ggplot2-drawing/bar/bar.R
+++ ggplot2-drawing/bar/bar.R
@@ -7,3 +7,6 @@ p <- ggplot(a,aes(x=gene,y=RPKM,fill=group))+geom_bar(stat="identity",width = .8
    scale_fill_discrete(labels=c(expression(paste(EGFR^"+","/",BM^"+")),expression(paste(EGFR^"+","/",BM^"-")),
                                 expression(paste(EGFR^"-","/",BM^"-"))))+
    geom_errorbar(aes(ymin=RPKM-se, ymax=RPKM+se),width=.1,position=position_dodge(.9))
+
+# p+ geom_text(aes(label=RPKM),vjust=-0.5,hjust=-0.35,position=position_dodge(1),angle=90)
+#angle=90，可以旋转text
