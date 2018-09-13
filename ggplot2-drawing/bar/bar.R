631f6d6a6e8f3d2d709b7d1c0f4dc7f5a6ead3aa
zhengzhouxia <35135685+zhengzhouxia@users.noreply.github.com>
2018-09-13 10:09:43 +0800

    Create bar.R

---
+++ ggplot2-drawing/bar/bar.R
@@ -0,0 +1,9 @@
+options(stringsAsFactors = F)
+library(ggplot2)
+a = read.table('exosomes.txt', header = T)
+
+p <- ggplot(a,aes(x=gene,y=RPKM,fill=group))+geom_bar(stat="identity",width = .85,position = "dodge")
+ p+ geom_text(aes(label=RPKM),vjust=-0.5,hjust=-0.35,position=position_dodge(1))+
+   scale_fill_discrete(labels=c(expression(paste(EGFR^"+","/",BM^"+")),expression(paste(EGFR^"+","/",BM^"-")),
+                                expression(paste(EGFR^"-","/",BM^"-"))))+
+   geom_errorbar(aes(ymin=RPKM-se, ymax=RPKM+se),width=.1,position=position_dodge(.9))
